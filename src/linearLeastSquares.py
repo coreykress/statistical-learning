5400dc3c8dde4893be896c4957fe1b43147022a4
Corey Kress <coreykre@buffalo.edu>
2018-03-25 23:18:19 -0400

    multi variable, pandas reading my csv as string now, so thats annoying

--- src/linearLeastSquares.py
+++ src/linearLeastSquares.py
@@ -5,10 +5,10 @@ import matplotlib.pyplot as plt
 
 class LinearLeastSquares():
 
-    def __init__(self, file_path, iterations, learning_rate):
+    def __init__(self, file_path, feature_columns, iterations, learning_rate):
         self.file_path = file_path
-        self.data_frame = pd.read_csv(file_path)
-        self.X = self.data_frame.loc[:, ["sepal_length", "sepal_width", "petal_length", "petal_width"]]
+        self.data_frame = pd.read_csv(file_path, names=feature_columns)
+        self.X = self.data_frame.loc[:, feature_columns]
         self.iterations = iterations
         self.learning_rate = learning_rate
         self.ones = np.ones([self.X.shape[0], 1])
@@ -17,6 +17,7 @@ class LinearLeastSquares():
         x_data = self.data_frame.loc[:, x_col].values.reshape(-1, 1)
         y_data = self.data_frame.loc[:, y_col].values.reshape(-1, 1)
         x_data = np.concatenate([self.ones, x_data], 1)
+        print(x_data)
         theta = np.array([[0.0, 0.0]])
         g, cost = self.gradient_descent(x_data, y_data, theta, self.learning_rate, self.iterations)
 
@@ -27,17 +28,31 @@ class LinearLeastSquares():
         plt.plot(x_vals, y_vals, '--')
         plt.show()
 
+    def display_multi_variable_regression(self):
+        norm_data = self.normalize_data(self.X)
+        norm_data = np.concatenate([self.ones, norm_data], 1)
+
+        x_data = norm_data[:, :-1]
+        y_data = norm_data[:, 2:3]
+        theta = np.zeros([1, 3])
+
+        g, cost = self.gradient_descent(x_data, y_data, theta, self.learning_rate, self.iterations)
+
     def compute_cost(self, x_data, y, theta):
         inner = np.power(((x_data.dot(theta.transpose())) - y), 2)
         return np.sum(inner) / (2 * len(x_data))
 
+    def normalize_data(self, data):
+        data = (data - data.mean()) / data.std()
+        return data;
+
     def gradient_descent(self, x_data, y_data, theta, alpha, iterations):
-        cost = 0
+        cost = np.zeros(iterations)
         for i in range(iterations):
             alpha_len = alpha / len(x_data)
-            theta -= alpha_len * (np.sum(x_data.dot(theta.transpose()) - y_data * x_data, axis=0))
-            cost = self.compute_cost(x_data, y_data, theta)
+            theta = theta - alpha_len * (np.sum(x_data * (x_data.dot(theta.transpose()) - y_data)))
+            cost[i] = self.compute_cost(x_data, y_data, theta)
             if i % 10 == 0:
-                print(cost)
+                print(cost[i])
 
         return theta, cost
--- src/run_lls.py
+++ src/run_lls.py
@@ -6,6 +6,19 @@ a fairly decent linear regression
 iterations = 1000
 learning rate = .00003
 thetas = [0,0]
+["sepal_length", "sepal_width", "petal_length", "petal_width"]
 """
-plot = lls("../data/iris_training.csv", 1000, .00003)
+plot = lls("../data/iris_training.csv", ["sepal_length", "sepal_width", "petal_length", "petal_width"], 1000, .00003)
 plot.display_one_dimensional("sepal_length", "sepal_width")
+
+
+"""
+multivariable linear regression
+
+iterations
+learning_rate
+thetas
+[]
+"""
+# plot = lls("../data/homes.txt", ["size","bedroom","price"], 1000, .01)
+# plot.display_multi_variable_regression()
